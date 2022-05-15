80caafea096a8ad3c912b28e911b1b0f07d44a63
Ryan Heuser <ryan.heuser@gmail.com>
2022-05-15 11:49:39 +0100

    updates

--- dialectics/dialectics.py
+++ dialectics/dialectics.py
@@ -15,6 +15,7 @@ def sync(corpora=INIT_DB_WITH_CORPORA,reverse=False):
 
 
 ## convenient names/funcs
-DB = tspace = TS = textspace = Textspace()
-find_texts = find = tspace.find
-search_texts = lookfor_texts = lookfor = look_for = look = tspace.look
+BaseText.textspace = BaseText.db = BaseText.tspace = BaseText.tdb = tspace = TS = textspace = Textspace()
+BaseText.relspace = BaseText.rdb = rspace = RS = relspace = Relspace()
+Text.find = BaseText.find = find_texts = find = tspace.find
+BaseText.lookfor = BaseText.search = search_texts = lookfor_texts = lookfor = look_for = look = tspace.look
